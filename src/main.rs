d1e007de6546846356a2faf33a448efb60161ec3
Mateus Costa <mateuscomp@gmail.com>
2020-03-07 21:45:27 +0100

    #2 Le arquivo WINDOWS_1252 e gera UTF-8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::Read;
+use encoding_rs_io::DecodeReaderBytesBuilder;
+use encoding_rs::WINDOWS_1252;
+
 fn main() {
-    println!("Hello, world!");
+    parse_html();
+}
+
+fn parse_html() {
+    let file_name = "resources/LeisMunicipais-com-br-Decreto-1-1984.html";
+    let file = File::open(file_name).unwrap(); // TODO: handle error here
+    let mut transcoded = DecodeReaderBytesBuilder::new()
+        .encoding(Some(WINDOWS_1252))
+        .build(file);
+
+    let mut dest = String::new();
+    transcoded.read_to_string(&mut dest).unwrap(); // TODO: handle error here
+    println!("{:?}", dest);
+}
+
+// TODO: make path dynamic
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn should_read_html_and_print_it() {
+        assert_eq!(1, 1);
+    }
 }
