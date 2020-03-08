0f0d2f55f5585aeb2bfa2a8ae22ecfb37fb79777
Mateus Costa <mateuscomp@gmail.com>
2020-03-08 21:44:45 +0100

    #2 Deserializa HTML para lei

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+#[macro_use]
+extern crate unhtml_derive;
+extern crate unhtml;
+
 use encoding_rs::WINDOWS_1252;
 use encoding_rs_io::DecodeReaderBytesBuilder;
 use std::fs::File;
 use std::io::Read;
+use unhtml::FromHtml;
 
 fn main() {
-    parse_html();
+    println!("{:?}", parse_html_to_lei());
 }
 
-fn parse_html() {
+#[derive(FromHtml, Debug, PartialEq)]
+struct Lei {
+    #[html(selector = "h2", attr = "inner")]
+    titulo: String,
+    // categoria: String,
+}
+
+fn parse_html_to_lei() -> Lei {
     let file_name = "resources/LeisMunicipais-com-br-Decreto-1-1984.html";
     let file = File::open(file_name).unwrap(); // TODO: handle error here
     let mut transcoded = DecodeReaderBytesBuilder::new()
@@ -16,16 +28,20 @@ fn parse_html() {
 
     let mut dest = String::new();
     transcoded.read_to_string(&mut dest).unwrap(); // TODO: handle error here
-    println!("{:?}", dest);
+
+    Lei::from_html(&dest)
+            .unwrap()
 }
 
 // TODO: make path dynamic
 
 #[cfg(test)]
 mod test {
+    use crate::{parse_html_to_lei, Lei};
 
     #[test]
     fn should_read_html_and_print_it() {
-        assert_eq!(1, 1);
+        assert_eq!(parse_html_to_lei(),
+                   Lei { titulo: "DECRETO Nº 1/84, de 05 de janeiro de 1984".to_string() });
     }
 }
