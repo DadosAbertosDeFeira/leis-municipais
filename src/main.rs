8c51444f17bbc807576abbed088b01e0f1dbb355
Mateus Costa <mateuscomp@gmail.com>
2020-03-14 12:24:07 +0100

    #2 Aplica cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ use crate::deserialize::parse_html_to_lei;
 mod deserialize;
 
 fn main() {
-        parse_html_to_lei("resources/LeisMunicipais-com-br-Lei-Complementar-122-2019.html");
+    parse_html_to_lei("resources/LeisMunicipais-com-br-Lei-Complementar-122-2019.html");
 }
-
