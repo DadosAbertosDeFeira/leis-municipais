bc936ea2f15cdc6cb778d3f03cb6e4883d6262fa
Mateus Costa <mateuscomp@gmail.com>
2020-03-28 20:29:14 +0100

    Implementa versão inicial do parser
    * 2 Adiciona novo projeto e integração continua
    
    * 2 Coloca configuração de CI no lugar certo
    
    * #2 Atualiza email in .toml
    
    * #2 Le arquivo WINDOWS_1252 e gera UTF-8
    
    * #2 Aplica cargo fmt
    
    * #2 Deserializa HTML para lei
    
    * #2 Aplica cargo fmt
    
    * #2 Usa regex para parsear titulo e resumo
    
    * #2 Aplica cargo fmt
    
    * #2 Parsea texto e documento
    
    * #2 Extrai arquivo como parametro
    
    * #2 Parsea arquivos sem link de documento
    
    * #2 Aplica cargo fmt
    
    * #2 Extrai deserialise para modulo especifico
    
    * #2 Aplica cargo fmt
    
    * #2 Ler pasta e subpastas
    
    * #2 Aplica cargo fmt
    
    * #2 Aplica cargo clippy
    
    * #2 Tira tags html do texto
    
    * #2 Aplica cargo fmt
    
    * #2 Adiciona categoria
    
    * #2 Aplica cargo fmt
    
    * # Adiciona categoria como parametro de parseamento
    
    * #2 Cria configuração de teste de integração
    
    * #2 Avanço no teste de integração e path como parametro
    
    * #2 Aplica cargo fmt
    
    * #2 Checa por valores especificos
    
    * #2 Aplica cargo fmt
    
    * #2 Faz teste de integração passar no CI
    
    * #2 Mostra informações com mais simplicidade
    
    * #2 Mostra informações com mais simplicidade
    
    * #2 Escreve leis para arquivo json
    
    * #2 Aplica cargo fmt
    
    * #2 Adiciona arquivo de teste de integração
    
    * #2 Compara json value ao inves de string
    
    * #2 Compara elemento por elemento para ordem não fazer diferença
    
    * #2 Usa e aplica clippy com modo pedantico
    
    * #2 Usa serde para escrever no arquivo ao inves da biblioteca padrão

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use crate::parser::parse_html_to_lei;
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use walkdir::{DirEntry, WalkDir};
+
+mod parser;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let folder_path = &args[1]; // TODO: error handler
+
+    let walker = WalkDir::new(folder_path).into_iter();
+
+    let mut directories = HashMap::new();
+    let mut current_directory = String::new();
+    let mut leis = Vec::new();
+    for entry in walker.filter_entry(|entry| is_not_hidden(entry)).skip(1) {
+        let entry = entry.unwrap();
+
+        if entry.file_type().is_dir() {
+            current_directory = entry.file_name().to_os_string().into_string().unwrap();
+
+            directories.insert(current_directory.clone(), 0);
+        } else if entry.file_type().is_file()
+            && entry.file_name().to_string_lossy().ends_with(".html")
+        {
+            let lei = parse_html_to_lei(
+                entry.path().to_str().unwrap(), // TODO: handle error
+                current_directory.to_string(),
+            );
+            // TODO: não usar vector para armazenar as leis
+            leis.push(lei);
+
+            *directories.get_mut(&current_directory).unwrap() += 1;
+        }
+    }
+
+    // TODO: escrever em formato de tabela igual no futiba
+    for (directory, files_number) in &directories {
+        println!("diretorio {}: {} arquivos lidos", directory, files_number);
+    }
+
+    let leis_file = File::create("leis.json").expect("Unable to create file");
+    serde_json::to_writer(leis_file, &leis).expect("Unable to write data");
+}
+
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map_or(false, |s| entry.depth() == 0 || !s.starts_with('.'))
+}
---
+++ tests/integration.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+#[test]
+fn should_parser_folder_and_write_leis_to_file_as_json() {
+    let mut cmd = Command::cargo_bin("leis-municipais").unwrap();
+    cmd.arg("resources/integration_tests/leis");
+
+    cmd.assert().stdout(predicate::str::contains(
+        "diretorio orgânica: 1 arquivos lidos",
+    ));
+    cmd.assert()
+        .stdout(predicate::str::contains("complementar: 2 arquivos lidos"));
+
+    let actual_content =
+        fs::read_to_string("leis.json").expect("Something went wrong reading the file");
+    let received_leis: serde_json::Value = serde_json::from_str(&actual_content).unwrap();
+    let expected_content = fs::read_to_string("resources/integration_tests/leis.json")
+        .expect("Something went wrong reading the file");
+    let expected_leis: serde_json::Value = serde_json::from_str(&expected_content).unwrap();
+
+    match (received_leis, expected_leis) {
+        (serde_json::Value::Array(received_leis), serde_json::Value::Array(expected_leis)) => {
+            received_leis.into_iter().for_each(|lei| {
+                assert!(expected_leis.contains(&lei));
+            })
+        }
+        _ => println!("unexpected type"),
+    }
+}
